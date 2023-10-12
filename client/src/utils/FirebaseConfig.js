bc541c4aa9acb723445696477eec972d2bea5e44
Felix Ouma <felix.ouma@wezacare.org>
2023-10-12 11:27:23 +0300

    feat: :sparkles: set up the project

---
+++ client/src/utils/FirebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAYPSTWjAuiC4nQUGaM7TTupMCQVieU5k",
+  authDomain: "whats-app-clone-1-0.firebaseapp.com",
+  projectId: "whats-app-clone-1-0",
+  storageBucket: "whats-app-clone-1-0.appspot.com",
+  messagingSenderId: "10554949333",
+  appId: "1:10554949333:web:5327f429c78fe1fd056c5c",
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(app);
