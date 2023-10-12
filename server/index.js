b8a942e75335638b8602789e7f2a93576c08b8ab
Felix Ouma <felix.ouma@wezacare.org>
2023-10-12 12:03:28 +0300

    feat(login): :sparkles: Implement login both frontend and backend

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import AuthRoutes from "./routes/AuthRoutes.js";
 
 dotenv.config();
 const app = express();
 
+const PORT = process.env.PORT || 3005;
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+app.use("/api/auth", AuthRoutes);
 
-
-app.listen(PORT, () => { 
-    console.log(`Server started on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
 });
