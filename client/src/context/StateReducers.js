42708037651f21763746eb5193ba7e0e85d0ce4d
Felix Ouma <felix.ouma@wezacare.org>
2023-10-29 13:14:25 +0300

    Add video call, voice call, end call, incoming voice call, and incoming video call constants and state reducers to the context.

--- client/src/context/StateReducers.js
+++ client/src/context/StateReducers.js
@@ -12,6 +12,10 @@ export const initialState = {
   userContacts: [],
   filteredContacts: [],
   searchTerm: "",
+  videoCall: undefined,
+  voiceCall: undefined,
+  incomingVoiceCall: undefined,
+  incomingVideoCall: undefined,
 };
 
 const reducer = (state, action) => {
@@ -86,6 +90,39 @@ const reducer = (state, action) => {
         ...state,
         searchTerm: action.searchTerm,
         filteredContacts: filteredContacts,
+      };
+    }
+    case reducerCases.SET_VIDEO_CALL: {
+      return {
+        ...state,
+        videoCall: action.videoCall,
+      };
+    }
+    case reducerCases.SET_VOICE_CALL: {
+      return {
+        ...state,
+        voiceCall: action.voiceCall,
+      };
+    }
+    case reducerCases.END_CALL: {
+      return {
+        ...state,
+        videoCall: undefined,
+        voiceCall: undefined,
+        incomingVoiceCall: undefined,
+        incomingVideoCall: undefined,
+      };
+    }
+    case reducerCases.SET_INCOMING_VOICE_CALL: {
+      return {
+        ...state,
+        incomingVoiceCall: action.incomingVoiceCall,
+      };
+    }
+    case reducerCases.SET_INCOMING_VIDEO_CALL: {
+      return {
+        ...state,
+        incomingVideoCall: action.incomingVideoCall,
       };
     }
     default: {
